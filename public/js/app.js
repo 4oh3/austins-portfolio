19f2ce12acfa33283e3bd52496f0434a654c3666
Austin <austinsmiht@gmail.com>
2018-04-19 23:15:08 -0600

    Added Privacy Policy

--- public/js/app.js
+++ public/js/app.js
@@ -390,6 +390,26 @@ $('.bootstrap').click(function () {
     console.log('clicked!');
 });
 
+/* - - - - - - - - - - - - - - - - - - - - - -
+Privacy Policy
+- - - - - - - - - - - - - - - - - - - - - - */
+
+(function (w, d) {
+    var loader = function loader() {
+        var s = d.createElement("script"),
+            tag = d.getElementsByTagName("script")[0];
+
+        s.src = "//cdn.iubenda.com/iubenda.js";
+        tag.parentNode.insertBefore(s, tag);
+    };if (w.addEventListener) {
+        w.addEventListener("load", loader, false);
+    } else if (w.attachEvent) {
+        w.attachEvent("onload", loader);
+    } else {
+        w.onload = loader;
+    }
+})(window, document);
+
 /***/ }),
 /* 2 */
 /***/ (function(module, exports) {
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -370,3 +370,19 @@ $('.bootstrap').click(function(){
     window.location = "http://austintsmith.ca/html-css-project";
     console.log('clicked!');
 });
+
+/* - - - - - - - - - - - - - - - - - - - - - -
+Privacy Policy
+- - - - - - - - - - - - - - - - - - - - - - */
+
+(function (w,d) {var loader = function () {
+    var
+    s = d.createElement("script"),
+    tag = d.getElementsByTagName("script")[0];
+
+    s.src = "//cdn.iubenda.com/iubenda.js";
+    tag.parentNode.insertBefore(s,tag);
+    }; if(w.addEventListener){w.addEventListener("load", loader, false);
+    } else if(w.attachEvent){w.attachEvent("onload", loader);
+    } else{w.onload = loader;
+    }})(window, document);
